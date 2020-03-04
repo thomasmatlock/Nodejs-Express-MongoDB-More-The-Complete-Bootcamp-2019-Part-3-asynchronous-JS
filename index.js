69bd6a84b6c2cc8234f4fce49758aeb1ae5b3994
Tom <metaphorps@gmail.com>
2020-03-03 21:39:49 -0600

    ...

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-	console.log(`Breed: ${data}`);
+    console.log(`Breed: ${data}`);
+    superagent.get(`https://dog.ceo/api/${data}/image/random`).end((err, res) => {
+        console.log(res.body);
+    }); // http Get request
 });
